a1a3c1a154c2f6e7bc18b3f4264a39e9de130ad8
hadeediqbal <hadeediqbal@yahoo.com>
2021-04-26 20:21:09 -0500

    added server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+const apiRoutes = require('./routes/apiRoute');
+
+const htmlRoutes = require('./routes/htmlRoute');
+
+const PORT = process.env.PORT || 3001;
+
+
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static('public'));
+
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
